44c76095339dbb8ca6514e26952de0b0c16e4a8c
Felipe Osorio <faosorios@gmail.com>
2020-08-12 13:05:10 -0400

    Add files via upload

--- R/norms.R
+++ R/norms.R
@@ -5,37 +5,55 @@ matrix.norm <- function(x, type = "Frobenius")
   if (is.data.frame(x))
     x <- as.matrix(x)
   if (!is.matrix(x))
-    stop("supply a matrix-like 'x'")
+    stop("supply a matrix-like 'x'.")
   if (!is.numeric(x))
-    stop("argument x is not a numeric matrix" )
+    stop("argument x is not a numeric matrix.")
+
+  job <- switch(type, "Frobenius" = "F",
+                      "1"         = "1",
+                      "inf"       = "I",
+                      "maximum"   = "M")
 
   storage.mode(x) <- "double"
-  dx <- dim(x)
+  z <- norm(x, type = job)
+  z
+}
 
-  switch(type,
-         "1" = {
-           z <- .Fortran("maxcol_norm",
-                         x = x,
-                         ldx = as.integer(dx[1]),
-                         n = as.integer(dx[1]),
-                         p = as.integer(dx[2]),
-                         val = as.double(0))$val
-          },
-          "Frobenius" = {
-            n <- prod(dx)
-            z <- .C("norm_two",
-                    x = x,
-                    inc = as.integer(1),
-                    n = as.integer(n),
-                    val = as.double(0))$val
-          },
-          "inf" = {
-            z <- .Fortran("maxrow_norm",
-                          x = x,
-                          ldx = as.integer(dx[1]),
-                          n = as.integer(dx[1]),
-                          p = as.integer(dx[2]),
-                          val = as.double(0))$val
-          })
+minkowski <- function(x, p = 2)
+{ ## computes a p-norm
+  if (!is.vector(x))
+    stop("argument x must be a vector.")
+  if (!is.numeric(x))
+    stop("argument x is not a numeric vector.")
+  if (p < 1)
+    stop("p must be greather or equal than 1.")
+
+  n <- length(x)
+  if (p == 1) {
+    z <- .C("norm_one",
+            x   = as.double(x),
+            inc = as.integer(1),
+            n   = as.integer(n),
+            val = as.double(0))$val
+  } else if (p == 2) {
+    z <- .C("norm_two",
+            x   = as.double(x),
+            inc = as.integer(1),
+            n   = as.integer(n),
+            val = as.double(0))$val
+  } else if (p == Inf) {
+    z <- .C("norm_inf",
+            x   = as.double(x),
+            inc = as.integer(1),
+            n   = as.integer(n),
+            val = as.double(0))$val
+  } else {
+    z <- .C("norm_minkowski",
+            x   = as.double(x),
+            inc = as.integer(1),
+            n   = as.integer(n),
+            p   = as.double(p),
+            val = as.double(0))$val
+  }
   z
 }
