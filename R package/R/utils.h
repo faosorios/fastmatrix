d67da0966c6dbbf969bcfd655cde375de0c8f0c9
Felipe Osorio <faosorios@gmail.com>
2020-08-16 17:26:20 -0400

    Add files via upload

---
+++ R package/R/utils.h
@@ -0,0 +1,14 @@
+/* $ID: utils.h, last updated 2020-08-10, F.Osorio */
+
+#ifndef FASTMAT_UTILS_H
+#define FASTMAT_UTILS_H
+
+#include "base.h"
+
+/* operations on vectors */
+extern void F77_NAME(hadamard)(double *, double *, int *, double *);
+extern void F77_NAME(inner_frobenius)(double *, int *, double *, int *, int *, int *, double *);
+extern void F77_NAME(sweepop)(double *, int *, int *, int *, double *, int *, int *);
+extern void F77_NAME(vech)(double *, int *, int *, double *);
+
+#endif /* FASTMAT_UTILS_H */
