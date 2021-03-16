b1f245daf9842013a5db44aa30fddfbeeeb0f362
Felipe Osorio <faosorios@gmail.com>
2021-03-16 10:17:07 -0300

    Add files via upload

---
+++ pkg/R/moments.R
@@ -0,0 +1,52 @@
+## ID: moments.R, last updated 2021-03-16, F.Osorio
+
+moments <- function(x)
+{ ## geometric mean using a compensated product scheme
+  if (!is.vector(x))
+    stop("argument 'x' must be a vector.")
+  if (!is.numeric(x))
+    stop("argument 'x' is not a numeric vector.")
+
+  # removing NA's
+  ok <- !is.na(x)
+  x <- x[ok]
+
+  n <- length(x)
+  z <- .C("central_moments",
+          x = as.double(x),
+          n = as.integer(n),
+          mean = double(1),
+          var = double(1),
+          m3 = double(1),
+          m4 = double(1))[c("mean","var","m3","m4")]
+  correction <- n / (n - 1)
+  s <- sqrt(correction * z$var)
+  z <- list(second = z$var, third = z$m3, fourth = z$m4, skewness = z$m3 / s^3,
+            kurtosis = z$m4 / s^4 - 3)
+  z
+}
+
+geomean <- function(x)
+{ ## geometric mean using a compensated product scheme
+  if (!is.vector(x))
+    stop("argument 'x' must be a vector.")
+  if (!is.numeric(x))
+    stop("argument 'x' is not a numeric vector.")
+
+  # removing NA's
+  ok <- !is.na(x)
+  x <- x[ok]
+
+  if (any(x <= 0)) {
+    warning("Non-positive values in 'x'.")
+    return(NA)
+  }
+
+  n <- length(x)
+  y <- x^(1 / n)
+  z <- .C("geometric_mean",
+          y = as.double(y),
+          n = as.integer(n),
+          mean = double(1))$mean
+  z
+}
